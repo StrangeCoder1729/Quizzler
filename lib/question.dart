68b393cf2c3fe958c251e394911c463cadadf562
Saket <saketisofficial05@gmail.com>
2022-07-10 07:48:52 +0530

    Commited the files

---
+++ lib/question.dart
@@ -0,0 +1,16 @@
+class Question{
+
+  late String questionText;
+  late bool questionAnswer;
+
+  // Question(String? q, bool a){
+  //
+  //   questionText = q!;
+  //   questionAnswer = a ;
+  //
+  // }
+
+  Question(this.questionText, this.questionAnswer);
+
+
+}
